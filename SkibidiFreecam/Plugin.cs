4fc7e46986b427d309fccf06bd567ea8046bdd03
striker67 <86896895+Striker-67@users.noreply.github.com>
2024-11-18 17:54:34 -0600

    Added CONST

--- SkibidiFreecam/Plugin.cs
+++ SkibidiFreecam/Plugin.cs
@@ -5,71 +5,49 @@ using Utilla;
 
 namespace SkibidiFreecam
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-    [ModdedGamemode]
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool inRoom;
+        public GameObject Head;
+        public GameObject HandL;
+        public GameObject HandR;
 
         void Start()
         {
-            /* A lot of Gorilla Tag systems will not be set up when start is called /*
-			/* Put code in OnGameInitialized to avoid null references */
-
-            Utilla.Events.GameInitialized += OnGameInitialized;
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
         }
 
         void OnEnable()
         {
-            /* Set up your mod here */
-            /* Code here runs at the start and whenever your mod is enabled*/
-
             HarmonyPatches.ApplyHarmonyPatches();
         }
 
         void OnDisable()
         {
-            /* Undo mod setup here */
-            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
-            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
-
             HarmonyPatches.RemoveHarmonyPatches();
         }
 
-        void OnGameInitialized(object sender, EventArgs e)
+        void OnGameInitialized()
         {
-            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+            Head = new GameObject();
+            HandL = new GameObject();
+            HandR = new GameObject();
+
+            HandL.transform.parent = Camera.main.transform;
+            HandR.transform.parent = Camera.main.transform;
+            HandleRigCONST();
+        }
+
+        public void HandleRigCONST()
+        {
+            GorillaTagger.Instance.offlineVRRig.head.overrideTarget = Head.transform;
+            GorillaTagger.Instance.offlineVRRig.leftHand.overrideTarget = HandL.transform;
+            GorillaTagger.Instance.offlineVRRig.rightHand.overrideTarget = HandR.transform;
         }
 
         void Update()
         {
-            /* Code here runs every frame when the mod is enabled */
-        }
-
-        /* This attribute tells Utilla to call this method when a modded room is joined */
-        [ModdedGamemodeJoin]
-        public void OnJoin(string gamemode)
-        {
-            /* Activate your mod here */
-            /* This code will run regardless of if the mod is enabled*/
-
-            inRoom = true;
-        }
-
-        /* This attribute tells Utilla to call this method when a modded room is left */
-        [ModdedGamemodeLeave]
-        public void OnLeave(string gamemode)
-        {
-            /* Deactivate your mod here */
-            /* This code will run regardless of if the mod is enabled*/
-
-            inRoom = false;
+          
         }
     }
 }
