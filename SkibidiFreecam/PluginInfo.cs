1eb79017e14b7669f8413632ebf93813436c0487
AritificialGorillas <rodericgickle@gmail.com>
2024-11-18 15:29:23 -0800

    sigma
    asdasd

---
+++ SkibidiFreecam/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace SkibidiFreecam
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.sadlycody.gorillatag.skibidifreecam";
+        public const string Name = "SkibidiFreecam";
+        public const string Version = "1.0.0";
+    }
+}
